aac22e6f2c70a796d894f03df12d0db6dfdb795b
girisai2020 <k.giridharan.0588@gmail.com>
2020-06-23 19:14:11 -0400

    Feature 1

--- app/src/main/java/com/example/usbank/MainActivity.kt
+++ app/src/main/java/com/example/usbank/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.lifecycle.Observer
 import com.example.usbank.posts.PostAdapter
 import com.example.usbank.posts.PostViewModel
 
+// New Feature
 class MainActivity : AppCompatActivity() {
 
     val viewModel: PostViewModel by viewModels()
